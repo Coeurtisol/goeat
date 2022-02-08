372e104d6ed1889ac6183e89084ad56e33b76e36
Choquet charles <chqtcharles@gmail.com>
2022-02-08 11:09:15 +0100

    fix controller restaurant

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -7,6 +7,7 @@ use App\Form\RestaurantType;
 use App\Repository\RestaurantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
